dbd0c320cd8a65f7261ea173a90ddcaff1ac0538
MTS <40478665@live.napier.ac.uk>
2023-05-12 00:00:22 +0630

    Initial Commit

---
+++ MovieAppSwiftUIPADC/MovieAppSwiftUIPADCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftUIPADCApp.swift
+//  MovieAppSwiftUIPADC
+//
+//  Created by Myo Thandar soe on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftUIPADCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
